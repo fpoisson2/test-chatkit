f4b35effc3908c30a924d29e4248da9f6cd44927
fpoisson2 <francis.poisson2@gmail.com>
2025-11-01 19:43:40 -0400

    chore: retirer la bannière de statut du widget

--- frontend/src/components/my-chat/ChatKitHost.tsx
+++ frontend/src/components/my-chat/ChatKitHost.tsx
@@ -1,22 +1,15 @@
 import { ChatKit, type ChatKitControl } from "@openai/chatkit-react";
 
-import { ChatSessionStatusBanner } from "./ChatSessionStatusBanner";
-
 type ChatKitHostProps = {
   control: ChatKitControl;
   chatInstanceKey: number;
-  statusMessage: string | null;
-  statusTone: "info" | "error" | "loading";
 };
 
 export const ChatKitHost = ({
   control,
   chatInstanceKey,
-  statusMessage,
-  statusTone,
 }: ChatKitHostProps) => (
   <div className="chatkit-layout__widget">
-    <ChatSessionStatusBanner message={statusMessage} tone={statusTone} />
     <ChatKit
       key={chatInstanceKey}
       control={control}
