6b3d8365473b249e23108825a7d325a1e47909f9
fpoisson2 <francis.poisson2@gmail.com>
2025-11-21 14:02:03 -0500

    Fix single-line composer button alignment

--- frontend/src/chatkit/components/ChatKit.tsx
+++ frontend/src/chatkit/components/ChatKit.tsx
@@ -636,7 +636,7 @@ export function ChatKit({ control, options, className, style }: ChatKitProps): J
           </div>
           <div className="chatkit-composer-actions">
             {composer?.attachments?.enabled && (
-              <>
+              <div className="chatkit-attach-wrapper">
                 <input
                   ref={fileInputRef}
                   type="file"
@@ -657,7 +657,7 @@ export function ChatKit({ control, options, className, style }: ChatKitProps): J
                     <path d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48"></path>
                   </svg>
                 </button>
-              </>
+              </div>
             )}
             <button
               type="submit"
