7f91cf51dc71058955a9f3da739a2879b1fb2a56
Francis Poisson-Gagnon <francis.poisson2@gmail.com>
2026-08-14 09:01:45 -0400

    Update build.gradle.kts

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "9.1.0" apply false
+    id("com.android.application") version "9.1.1" apply false
 }
 
 // Build both APKs with: ./gradlew buildAll
